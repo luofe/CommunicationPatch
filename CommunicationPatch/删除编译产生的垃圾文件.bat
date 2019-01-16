0da02f9f0aa62bf3ee244e98491ac7c9ea2e8a44
liufeng <liufengxw@kdxfilm.com>
2019-01-16 11:55:39 +0800

    修改目录

--- CommunicationPatch/删除编译产生的垃圾文件.bat
+++ CommunicationPatch/删除编译产生的垃圾文件.bat
@@ -4,10 +4,4 @@ del /Q CommunicationPatch\Debug\List\*.*
 del /Q CommunicationPatch\Debug\Obj\*.*
 del /Q CommunicationPatch\*.dep
 
-del /Q CommunicationPatch_BootLoader\Debug\Exe\*.*
-del /Q CommunicationPatch_BootLoader\Debug\Exe\*.sim
-del /Q CommunicationPatch_BootLoader\Debug\List\*.*
-del /Q CommunicationPatch_BootLoader\Debug\Obj\*.*
-del /Q CommunicationPatch_BootLoader\*.dep
-
-pause
+exit
